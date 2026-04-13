4dcbd2743fb3b96cd89c52bf06dbc21f1645e7c4
fp-hitorigoto <all.the.best.yade@gmail.com>
2026-04-13 22:22:36 +0900

    Github Pges対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://fp-hitorigoto.vercel.app',
+  site: 'https://fp-hitorigoto.github.io',
+  base: '/fp-hitorigoto',
   integrations: [tailwind(), mdx()],
 });
